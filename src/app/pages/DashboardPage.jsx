ea80ae0432c4bddf5b8d6e4f7b7cbcd55d5fbd17
Aditya Meena <adityameena112@gmail.com>
2022-10-29 20:51:50 +0530

    dashboard changes

--- src/app/pages/DashboardPage.jsx
+++ src/app/pages/DashboardPage.jsx
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 class DashboardPage extends Component {
     state = {
         accountList: []
-    } 
+    }
 
     componentDidMount() {
         this.fetchAccountList()
@@ -15,18 +15,37 @@ class DashboardPage extends Component {
         HttpService.fetchUserAccountList().then((response) => {
             console.log(response.data);
             toast.success("Accounts: " + response.data?.length)
+            this.setState({ accountList: response.data })
         }).catch((error) => {
             toast.error("Unable to fetch account")
         })
     }
 
-    render() { 
+    render() {
         return (
             <div>
-                <h1>Dashboard</h1>
+                <div className="card no-accounts-card">
+                    <div className="card body">
+                        <h1 className="card-title">
+                            <i className="fas fa-ban text-danger"></i>No Registered Accounts
+                        </h1>
+                        <div className="card-text">
+                            You Currently do not have any registered accounts. <br />
+                            please click below to register / add a new account.
+
+                        </div>
+
+                        <br />
+
+                        <button id="" className="btn btn-primary btn-md shadow" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> <i className="fa fa-credit-card"></i> Add New Account
+                        </button>
+
+
+                    </div>
+                </div>
             </div>
         );
     }
 }
- 
+
 export default DashboardPage;
